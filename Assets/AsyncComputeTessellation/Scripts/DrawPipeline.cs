35fc23b18bfee8785c2a1980bd6943ba3958969f
Artem Vetik <av.unitygames@gmail.com>
2025-04-09 21:13:19 +0300

    Create basic draw pipeline

--- Assets/AsyncComputeTessellation/Scripts/DrawPipeline.cs
+++ Assets/AsyncComputeTessellation/Scripts/DrawPipeline.cs
@@ -1,9 +1,169 @@
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace AV.AsyncComputeTessellation
 {
     public class DrawPipeline : MonoBehaviour
     {
-        
+        [SerializeField] private ComputeShader _updateCS;
+        [SerializeField] private ComputeShader _copyDrawCS;
+        [SerializeField] private ComputeShader _vsPrepassCS;
+        [SerializeField] private Mesh _mesh;
+        [SerializeField] private Material _material;
+
+        private int _pingPongCounter = 0;
+        private int _subdCulledBuffIdx = 0;
+
+        private ComputeBuffer _subdIn;
+        private ComputeBuffer _subdOut;
+        private ComputeBuffer _subdOutCulled;
+        private ComputeBuffer[] _vsPrepassV = new ComputeBuffer[2];
+        private ComputeBuffer[] _vsPrepassIdx = new ComputeBuffer[2];
+        private ComputeBuffer[] _drawArgs = new ComputeBuffer[2];
+
+        private ComputeBuffer _subdCounter;
+        private ComputeBuffer _leafMeshVertex;
+        private ComputeBuffer _leafMeshIndex;
+        private ComputeBuffer _meshVertex;
+        private ComputeBuffer _meshIndex;
+
+        private ComputeBuffer _objectCB;
+        private ComputeBuffer _tessellationCB;
+        private ComputeBuffer _frameCB;
+
+        private unsafe void Start()
+        {
+            int subdSize = 1000000;
+
+            _subdIn = new ComputeBuffer(subdSize, sizeof(uint4), ComputeBufferType.Structured) { name = "SubdIn" };
+            _subdOut = new ComputeBuffer(subdSize, sizeof(uint4), ComputeBufferType.Structured) { name = "SubdOut" };
+            _subdOutCulled = new ComputeBuffer(subdSize, sizeof(uint4), ComputeBufferType.Structured) { name = "SubdOutCulled" };
+            _subdCounter = new ComputeBuffer(4, sizeof(uint), ComputeBufferType.Structured) { name = "SubdCounter" };
+
+            for (int i = 0; i < 2; i++)
+            {
+                _vsPrepassV[i] = new ComputeBuffer(subdSize, sizeof(VertexOut), ComputeBufferType.Structured) { name = $"VsPrepassV-{i}" };
+                _vsPrepassIdx[i] = new ComputeBuffer(subdSize, sizeof(uint), ComputeBufferType.Structured) { name = $"VsPrepassIdx-{i}" };
+                _drawArgs[i] = new ComputeBuffer(4, sizeof(uint), ComputeBufferType.IndirectArguments) { name = $"DrawArgs-{i}" };
+            }
+
+            var triCount = _mesh.GetIndices(0).Length / 3;
+            var subdData = new NativeArray<uint4>(triCount + 1, Allocator.Temp);
+
+            for (int i = 0; i < triCount; i++)
+                subdData[i] = new uint4(0, 0x1, (uint)i * 3, 1);
+
+            _subdIn.SetData(subdData);
+
+            for (int i = 0; i < triCount; i++)
+                subdData[i] = new uint4(0, 0, 0, 0);
+
+            _subdOut.SetData(subdData);
+
+            var counterData = new NativeArray<uint>(4, Allocator.Temp);
+            counterData[0] = (uint)triCount;
+            counterData[1] = counterData[2] = counterData[3] = 0;
+
+            _subdCounter.SetData(counterData);
+
+            _meshVertex = new ComputeBuffer(_mesh.vertexCount, sizeof(Vertex), ComputeBufferType.Structured) { name = "MeshVertex" };
+
+            var vertices = new NativeArray<Vertex>(_mesh.vertexCount, Allocator.Temp);
+            for (int i = 0; i < _mesh.vertexCount; i++)
+            {
+                vertices[i] = new Vertex()
+                {
+                    Position = new float4(_mesh.vertices[i].x, _mesh.vertices[i].y, _mesh.vertices[i].z, 0),
+                    Normal = new float4(_mesh.normals[i].x, _mesh.normals[i].y, _mesh.normals[i].z, 0),
+                    TangentU = _mesh.tangents[i],
+                    TexC = new float4(_mesh.uv[i].x, _mesh.uv[i].y, 0, 0)
+                };
+            }
+
+            _meshVertex.SetData(vertices);
+
+            _meshIndex = new ComputeBuffer(_mesh.GetIndices(0).Length, sizeof(uint));
+            _meshIndex.name = nameof(_meshIndex);
+            _meshIndex.SetData(_mesh.GetIndices(0));
+
+            var leafVertices = LeafMesh.GetLeafVertices(0);
+            var leafIndices = LeafMesh.GetLeafIndices(0);
+
+            _leafMeshVertex = new ComputeBuffer(leafVertices.Count, sizeof(float2)) { name = "LeafMeshVertex" };
+            _leafMeshIndex = new ComputeBuffer(leafIndices.Count, sizeof(uint)) { name = "LeafMeshIndex" };
+
+            _leafMeshVertex.SetData(leafVertices);
+            _leafMeshIndex.SetData(leafIndices);
+
+            var objData = ObjectData.CreateDefault();
+            var tessData = TessellationData.CreateDefault();
+
+            PerFrameData perFrameData;
+            perFrameData.ViewProj = Camera.main.projectionMatrix * Camera.main.worldToCameraMatrix;
+            perFrameData.CamPosition = Camera.main.transform.position;
+            perFrameData.PredictedCamPosition = Camera.main.transform.position;
+            perFrameData.DeltaTime = Time.deltaTime;
+            perFrameData.TotalTime = Time.time;
+            // TODO: perFrameData.FrustrumPlanes = ...
+
+            // TODO: Use NativeArray instead of List
+            _objectCB = new ComputeBuffer(1, sizeof(ObjectData), ComputeBufferType.Constant) { name = "ObjectCB" };
+            _objectCB.SetData(new List<ObjectData>() { objData });
+
+            _frameCB = new ComputeBuffer(1, sizeof(PerFrameData), ComputeBufferType.Constant, ComputeBufferMode.Dynamic) { name = "FrameCB" };
+            _frameCB.SetData(new List<PerFrameData>() { perFrameData });
+
+            _tessellationCB = new ComputeBuffer(1, sizeof(TessellationData), ComputeBufferType.Constant) { name = "TessellationCB" };
+            _tessellationCB.SetData(new List<TessellationData>() { tessData });
+        }
+
+        private unsafe void OnRenderObject()
+        {
+            int kernelHandleUpd = _updateCS.FindKernel("main");
+            int kernelHandlePrepass = _vsPrepassCS.FindKernel("main");
+            int kernelHandleCopyDraw = _copyDrawCS.FindKernel("main");
+
+            CommandBuffer cmd = new CommandBuffer { name = "Adaptive Tessellation" };
+
+            cmd.SetGlobalBuffer("SubdBufferIn", _pingPongCounter == 0 ? _subdIn : _subdOut);
+            cmd.SetGlobalBuffer("SubdBufferOut", _pingPongCounter == 0 ? _subdOut : _subdIn);
+            cmd.SetGlobalBuffer("SubdBufferOutCulled", _subdOutCulled);
+            cmd.SetGlobalBuffer("PrepassVertexOut", _subdCulledBuffIdx == 0 ? _vsPrepassV[0] : _vsPrepassV[1]);
+            cmd.SetGlobalBuffer("PrepassIndexOut", _subdCulledBuffIdx == 0 ? _vsPrepassIdx[0] : _vsPrepassIdx[1]);
+            cmd.SetGlobalBuffer("SubdCounter", _subdCounter);
+            cmd.SetGlobalBuffer("DrawArgs", _subdCulledBuffIdx == 0 ? _drawArgs[0] : _drawArgs[1]);
+            cmd.SetGlobalBuffer("MeshDataVertex", _meshVertex);
+            cmd.SetGlobalBuffer("MeshDataIndex", _meshIndex);
+            cmd.SetGlobalBuffer("LeafVertex", _leafMeshVertex);
+            cmd.SetGlobalBuffer("LeafIndex", _leafMeshIndex);
+
+            PerFrameData perFrameData;
+            perFrameData.ViewProj = Camera.main.projectionMatrix * Camera.main.worldToCameraMatrix;
+            perFrameData.CamPosition = Camera.main.transform.position;
+            perFrameData.PredictedCamPosition = Camera.main.transform.position;
+            perFrameData.DeltaTime = Time.deltaTime;
+            perFrameData.TotalTime = Time.time;
+
+            _frameCB.SetData(new List<PerFrameData>() { perFrameData });
+
+            cmd.SetGlobalConstantBuffer(_objectCB, "UnityObjectData", 0, sizeof(ObjectData));
+            cmd.SetGlobalConstantBuffer(_tessellationCB, "UnityTessellationData", 0, sizeof(TessellationData));
+            cmd.SetGlobalConstantBuffer(_frameCB, "UnityPerFrameData", 0, sizeof(PerFrameData));
+
+            cmd.DispatchCompute(_updateCS, kernelHandleUpd, 10000, 1, 1);
+            cmd.DispatchCompute(_vsPrepassCS, kernelHandlePrepass, 65000, 1, 1);
+            cmd.DispatchCompute(_copyDrawCS, kernelHandleCopyDraw, 1, 1, 1);
+
+            cmd.DrawProceduralIndirect(Matrix4x4.identity, _material, 0, MeshTopology.Triangles, _drawArgs[0]);
+
+            Graphics.ExecuteCommandBuffer(cmd);
+
+            _pingPongCounter = 1 - _pingPongCounter;
+
+            cmd.Release();
+        }
     }
 }
---
+++ Assets/AsyncComputeTessellation/Scripts/LeafMesh.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+namespace AV.AsyncComputeTessellation
+{
+    internal class LeafMesh
+    {
+        public static List<float2> GetLeafVertices(int level)
+        {
+            var vertices = new List<float2>();
+
+            float numRow = 1 << level;
+            float col = 0.0f, row = 0.0f;
+            float d = 1.0f / numRow;
+
+            while (row <= numRow)
+            {
+                while (col <= row)
+                {
+                    vertices.Add(new float2(col * d, 1.0f - row * d));
+                    col++;
+                }
+                row++;
+                col = 0;
+            }
+
+            return vertices;
+        }
+
+        public static List<uint> GetLeafIndices(int level)
+        {
+            var indices = new List<uint>();
+            uint col = 0, row = 0;
+            uint elem = 0, numCol = 1;
+            uint orientation;
+            int numRow = 1 << level;
+
+            Func<uint, uint3> newTriangle = (uint orientation) =>
+            {
+                if (orientation == 0)
+                    return new uint3(elem, elem + numCol, elem + numCol + 1);
+                else if (orientation == 1)
+                    return new uint3(elem, elem - 1, elem + numCol);
+                else if (orientation == 2)
+                    return new uint3(elem, elem + numCol, elem + 1);
+                else if (orientation == 3)
+                    return new uint3(elem, elem + numCol - 1, elem + numCol);
+                else
+                    throw new Exception("Bad orientation error");
+            };
+
+            while (row < numRow)
+            {
+                orientation = (row % 2 == 0) ? 0u : 2u;
+                while (col < numCol)
+                {
+                    var t = newTriangle(orientation);
+                    indices.Add(t.x);
+                    indices.Add(t.y);
+                    indices.Add(t.z);
+                    orientation = (orientation + 1) % 4;
+
+                    if (col > 0)
+                    {
+                        t = newTriangle(orientation);
+                        indices.Add(t.x);
+                        indices.Add(t.y);
+                        indices.Add(t.z);
+                        orientation = (orientation + 1) % 4;
+                    }
+                    col++;
+                    elem++;
+                }
+                col = 0;
+                numCol++;
+                row++;
+            }
+            return indices;
+        }
+    }
+}
